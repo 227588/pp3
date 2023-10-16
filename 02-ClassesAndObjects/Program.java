b1f693692c33d2990cff29da14d5e5b06fc17955
227588 <227588@student.uek.krakow.pl>
2023-10-16 21:06:49 +0200

    Before Class 02

---
+++ 02-ClassesAndObjects/Program.java
@@ -0,0 +1,12 @@
+public class Program{
+     public static void main(String[] args) {
+        String name = "Wojciech";
+        String surname = "Bubula";
+        int age = 21;
+        String studentId = "227588";
+        boolean firstYearStudent = false;
+        String studyField = "UEK";
+
+        System.out.println("Name: " + name + "\nSurname: " + surname + "\nage: " + age + "\nStudentId: " + studentId + "\nFirst year student: " + firstYearStudent + "\nStudy field: " + studyField);
+    }    
+}
